62e0f42f3d6e6ac474c576eafea9db596b2b319a
yuqisss <59928134+yuqisss@users.noreply.github.com>
2020-06-28 18:26:23 -0500

    pages

---
+++ public/comment1.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Stories</title>
+</head>
+
+<body>
+
+    <h1>Stories</h1>
+    <input type="submit" value="New Post"><br>
+
+</body>
+</html>
