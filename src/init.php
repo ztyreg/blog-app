a31adde4a41913928a8ed772d8233d9ccce46016
Ethan <ethzheng[at]gmail[dot]com>
2020-07-01 00:00:10 -0500

    ui

--- src/init.php
+++ src/init.php
@@ -1,7 +1,7 @@
 <?php
 
 require_once("myconfig.php");
-require_once("database.php");
+require_once("Database.php");
 require_once("functions.php");
 require_once("User.php");
 require_once("Session.php");
